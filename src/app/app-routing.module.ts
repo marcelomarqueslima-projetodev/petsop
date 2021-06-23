2c2fd73b8f8ca0b509a7583f7f4eff1cac3c8be7
Marcelo Marques Lima <marcelomarqueslima.dev@gmail.com>
2021-06-23 17:07:58 -0300

    Finalizando router da apliccação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { FramePageComponent } from './pages/account/master/frame-page';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 
 const routes: Routes = [
-    {
-        path: '', component: LoginPageComponent
-    }
+    { path: '', 
+      component: FramePageComponent, 
+      children: [
+          { path: '', component: ProductsPageComponent },
+          { path: 'cart', component: CartPageComponent }
+      ]
+    },
+    { path: 'account', 
+      component: FramePageComponent, 
+      children: [
+          { path: 'pets', component: PetsPageComponent }
+      ]
+    },
+    { path: 'login', component: LoginPageComponent },
+    { path: 'reset-password', component: ResetPasswordPageComponent },
+    { path: 'signup', component: SignupPageComponent },
  ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductsPageComponent } from './pages/store/products-page/products-page
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FramePageComponent } from './pages/account/master/frame-page';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ResetPasswordPageComponent,
     SignupPageComponent,
     ProductsPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    FramePageComponent
   ],
   imports: [
     BrowserModule,
